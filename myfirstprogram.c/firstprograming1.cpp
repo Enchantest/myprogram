f6c9938b0109e9c7b26ff044b42c8242abdbe864
Enchantest <lewohirene@gmail.com>
2025-04-16 10:24:11 -0400

    Add files via upload

---
+++ myfirstprogram.c/firstprograming1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+printf("Hello World!\n");
+printf("Have a good day!\n");
+printf("I am learning C.\n");
+printf("And it is awesome!\n");
+int myNum = 15;
+myNum = 15;
+printf("%d", myNum);
+return 0;
+}
+
