d61349102baaf57eb308c2ef4cdddd5267fed709
Enchantest <lewohirene@gmail.com>
2025-04-16 06:58:03 -0400

    Add files via upload

---
+++ myfirstprogram.c/FIRSTPROGRAMMING.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("hello world\n");
+	
+	printf("Have a Good Day\n");
+	
+	int myNum = 15;
+printf("i love the number: %d\n", myNum);
+    myNum = 10;
+printf("%\n", myNum);
+int x= 15;
+int y = 12;
+int sum = x*y;
+printf("%d",sum);
+return 0;
+}
